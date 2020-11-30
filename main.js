5d520918e4673629da5778c298ea755510b12c14
simon441 <simon.cateau@yahoo.fr>
2020-11-30 17:55:14 +0100

    Lesson3

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      description: 'These are wonderful woolly socks!',
+      image: './assets/images/socks_green.jpg',
+      url: 'http://localhost',
     }
   },
 })
